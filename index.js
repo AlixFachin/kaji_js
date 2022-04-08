ce68852d63854197d92ca3df7f80c25169a11324
Alix Fachin <alix.fachin@me.com>
2022-04-08 09:35:01 +0900

    Revisions and Suggestions

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
+// できるだけvarを使わない方がいいです。letやconstを使った方がいいです。
+//  配列の場合、内容が変わっても、配列が変わらないので const を使うのはおすすめです。
+
 function DeleteFromArray(Array1,item){
-  var Array2 = [];
-  for (var i=0; i < Array1.length; i++){
+  const Array2 = []
+  for (let i=0; i < Array1.length; i++){
     if(Array1[i] !== item){
       Array2.push(Array1[i])
       //console.log(Array1[i]);
@@ -11,36 +14,41 @@ function DeleteFromArray(Array1,item){
 }
 
 function SumArray(Array1){
-  var num = 0;
-  for (var i=0; i < Array1.length; i++){
-    num += Array1[i];
-  }
-  return num;
+  // var num = 0;
+  // for (var i=0; i < Array1.length; i++){
+  //   num += Array1[i];
+  // }
+  // return num;
+
+  // reduceはめちゃくちゃ便利な関数です。日本語で説明しにくいけど、配列の全てのアイテムをある処理を対応したい場合使えます。
+  // https://zukucode.com/2017/05/javascript-reduce.html
+  return Array1.reduce((prev, cur) => cur + prev, 0);
 }
 
 function MaxArray(Array1){
-  var num = 0;
-  for (var i=0; i < Array1.length; i++){
-    if(num < Array1[i]){
-      num = Array1[i];
-    }
-  }
-  return num;
+  // var num = 0;
+  // for (var i=0; i < Array1.length; i++){
+  //   if(num < Array1[i]){
+  //     num = Array1[i];
+  //   }
+  // }
+  // return num;
+  return Array1.reduce((prev, cur) => Math.max(prev, cur),0);
 }
 
 
 function IsEFone(AliceUtility,BobUtility,AliceAllocation,BobAllocation){
-  var AAU=[];
-  var BAU=[];
-  var ABU=[];
-  var BBU=[];
-  for (var i=0; i < AliceAllocation.length; i++){
+  const AAU=[];
+  const BAU=[];
+  const ABU=[];
+  const BBU=[];
+  for (let i=0; i < AliceAllocation.length; i++){
     AAU.push(AliceUtility[AliceAllocation[i]]);
     BAU.push(BobUtility[AliceAllocation[i]]);
   }
   //console.log("AAU",AAU);
   //console.log("BAU",BAU);
-  for (var i=0; i < BobAllocation.length; i++){
+  for (let i=0; i < BobAllocation.length; i++){
     ABU.push(AliceUtility[BobAllocation[i]]);
     BBU.push(BobUtility[BobAllocation[i]]);
   }
@@ -48,34 +56,33 @@ function IsEFone(AliceUtility,BobUtility,AliceAllocation,BobAllocation){
   //console.log("BBU",BBU);
   //console.log("SumArray(AAU)",SumArray(AAU));
   //console.log("SumArray(ABU) - Math.max(ABU)",SumArray(ABU) - MaxArray(ABU));
-  if (SumArray(AAU) >= SumArray(ABU) - MaxArray(ABU) && SumArray(BBU) >= SumArray(BAU)- MaxArray(BAU)) {
-    return 1;
-  }else{
-    return 0;
-  }
+  
+  // isEFOneを使う時に1か0にテストしますから、直接booleanを返しても問題ない
+  return (SumArray(AAU) >= SumArray(ABU) - MaxArray(ABU) && SumArray(BBU) >= SumArray(BAU)- MaxArray(BAU));
+  
 }
 
 
 
 function AdjustedWinner(AliceUtility,BobUtility){
-  var AliceAllocation = Array.from(Array(AliceUtility.length), (v, k) => k);
-  var BobAllocation = [];
-  var alist = [];
-  for (var i=0; i < AliceAllocation.length; i++){
+  const AliceAllocation = Array.from(Array(AliceUtility.length), (v, k) => k);
+  const BobAllocation = [];
+  const alist = [];
+  for (let i=0; i < AliceAllocation.length; i++){
     //console.log(isString(key));
     alist.push([AliceAllocation[i], AliceUtility[AliceAllocation[i]]/BobUtility[AliceAllocation[i]]]);
   }
-  console.log("AliceAllocation,BobAllocation",[AliceAllocation,BobAllocation]);
-  alist.sort(function(a,b){
-    if( a[1] > b[1] ) return -1;
-    if( a[1] < b[1] ) return 1;
-    return 0;
-  });
-  var t = 0;
-  for (var i=0; i < alist.length; i++){
-    if(IsEFone(AliceUtility,BobUtility,AliceAllocation,BobAllocation)==1){
+  //  ${}を使うと便利です。backquoteが必須です
+  console.log(`AliceAllocation: ${AliceAllocation}, BobAllocation: ${BobAllocation}`);
+  // sortにコールバックを使うと、1や-1と比較するではなく、>0 か <0 だけ比較するから、b[1]-a[1] が使えます。 
+  // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/sort 
+  alist.sort((a, b) => (b[1]-a[1]));
+  let t = 0;
+  for (let i=0; i < alist.length; i++){
+    if(IsEFone(AliceUtility,BobUtility,AliceAllocation,BobAllocation)){
       break;
     }
+    // ここに (t < alist.length) のテストが必要でしょうか？
     AliceAllocation = DeleteFromArray(AliceAllocation, alist[t][0]);
     BobAllocation.push(alist[t][0]);
     console.log("AliceAllocation,BobAllocation",[AliceAllocation,BobAllocation]);
